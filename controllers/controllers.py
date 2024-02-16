28920531ea59e29da37fbf329c687500c4b4d838
isidrovera <118472736+isidrovera@users.noreply.github.com>
2024-02-16 18:00:47 -0500

    ...

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -30,19 +30,22 @@ class AlquilerQRController(http.Controller):
 
 class PublicTicketController(http.Controller):
     # Ruta GET para mostrar el formulario
-    @http.route('/ticket/reportar_incidencia', type='http', auth="public", methods=['GET'], website=True, csrf=False)
+    @http.route('/ticket/reportar_incidencia', type='http', auth="public", methods=['GET'], website=True)
     def display_reportar_incidencia(self, **kw):
-        # Obtener datos necesarios para prellenar el formulario o cualquier otra lógica
+        # Suponiendo que obtienes el ID del registro a través de la URL
+        id_registro = kw.get('id_registro')
+        registro = request.env['alquiler'].sudo().search([('id', '=', int(id_registro))])
         values = {
-            # Suponiendo que hay lógica para obtener estos valores
-            'partner_id': ...,  # ID del cliente
-            'direccion': ...,   # Dirección
-            'contacto_id': ..., # ID de contacto
-            'celular': ...,     # Número de celular
-            'correo': ...,      # Correo electrónico
-            'product_id': ...,  # ID del producto
+            'partner_id': registro.cliente_id.id if registro.cliente_id else '',
+            'direccion': registro.direccion if registro.direccion else '',
+            'contacto_id': registro.contacto_id if registro.contacto_id else '',
+            'celular': registro.celular if registro.celular else '',
+            'correo': registro.correo_ if registro.correo_ else '',
+            'product_id': registro.id,
+            # Otros campos que necesitas pasar al formulario
         }
         return request.render('sat.reportar_incidencia_form', values)
+
 
     # Ruta POST para procesar el formulario
    # Asumiendo que las rutas son las mismas que las anteriores
