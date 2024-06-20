80c586ee12fef1a1dfe0bd983d46fcae104633a0
isidrovera <118472736+isidrovera@users.noreply.github.com>
2024-06-20 16:03:17 -0500

    Update transportistas.py

--- controllers/transportistas.py
+++ controllers/transportistas.py
@@ -6,15 +6,19 @@ _logger = logging.getLogger(__name__)
 
 class SatController(http.Controller):
 
-    @http.route('/sat/change_location/<int:record_id>', type='http', auth='public')
+    @http.route('/sat/change_location/<int:record_id>', type='http', auth='public', website=True)
     def change_location(self, record_id, **kwargs):
         _logger.info(f"Request received to change location for record ID: {record_id}")
-        record = request.env['sat.sat'].browse(record_id)
+        record = request.env['sat.sat'].sudo().browse(record_id)
         if record.exists():
-            _logger.info(f"Record found: {record.name} with current location {record.ubicacion_id}")
+            _logger.info(f"Record found: {record.name.name} with current location {record.ubicacion_id}")
             record.ubicacion_id = 'primer_piso'
             _logger.info(f"Location changed to 'primer_piso' for record ID: {record_id}")
-            return request.render('sat.location_change_success', {'record': record})
+            context = {
+                'record': record,
+                'website': request.env['website'].get_current_website() if request.env['website'].search([]) else None,
+            }
+            return request.render('sat.location_change_success', context)
         else:
             _logger.error(f"Record with ID {record_id} not found")
             return request.not_found()
