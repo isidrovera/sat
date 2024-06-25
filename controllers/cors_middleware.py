bd1e5ca2c57b888bc2da0331cec36e3972b6f92e
isidrovera <118472736+isidrovera@users.noreply.github.com>
2024-06-25 10:26:46 -0500

    Update cors_middleware.py

--- controllers/cors_middleware.py
+++ controllers/cors_middleware.py
@@ -4,10 +4,10 @@ from odoo.http import request
 class CorsMiddleware(http.Controller):
 
     @http.route('/api/<path:path>', type='http', auth='public', methods=['OPTIONS'], csrf=False)
-    def options(self, path):
+    def options(self, path, **kwargs):
         headers = {
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
             'Access-Control-Allow-Headers': 'Content-Type, Authorization'
         }
-        return request.make_response('OK', headers=headers)
+        return request.make_response('', headers=headers)
