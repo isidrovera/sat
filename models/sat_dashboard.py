b67aacbac3d4cc5f40fa199f14c505ee2b79401d
isidrovera <118472736+isidrovera@users.noreply.github.com>
2024-10-28 10:35:52 -0500

    Update sat_dashboard.py

--- models/sat_dashboard.py
+++ models/sat_dashboard.py
@@ -5,25 +5,64 @@ class SatDashboard(models.Model):
 
     @api.model
     def get_dashboard_data(self):
-        total_evaluaciones = self.env['evaluacion.personal'].search_count([])
-        total_reparaciones = self.env['reparaciones.reparaciones'].search_count([])
-        total_alquileres = self.env['ticket.alquiler'].search_count([])
-        total_maquinas_alquiler = self.env['alquiler'].search_count([('estado_alquiler_id', '=', 'alquilada')])
-        
-        # Datos adicionales para hoy
+        current_year = fields.Date.today().year
         today = fields.Date.today()
-        total_reparaciones_hoy = self.env['reparaciones.reparaciones'].search_count([('create_date', '=', today)])
-        total_alquileres_hoy = self.env['ticket.alquiler'].search_count([('agenda', '=', today)])
+
+        # Total de reparaciones por mes para el año actual
+        reparaciones_por_mes = self._get_data_por_mes('reparaciones.reparaciones', 'create_date', current_year)
+        
+        # Total de tickets por mes para el año actual
+        tickets_por_mes = self._get_data_por_mes('ticket.alquiler', 'agenda', current_year)
+        
+        # Reparaciones programadas para hoy
+        reparaciones_hoy = self.env['reparaciones.reparaciones'].search_count([('create_date', '=', today)])
+        
+        # Tickets programados para hoy
+        tickets_hoy = self.env['ticket.alquiler'].search_count([('agenda', '=', today)])
+        
+        # Evaluaciones de personal para el año actual
+        total_evaluaciones = self.env['evaluacion.personal'].search_count([('fecha', '>=', f'{current_year}-01-01'), ('fecha', '<=', f'{current_year}-12-31')])
+
+        # Reparaciones y tickets por técnico
+        reparaciones_por_tecnico = self._get_reparaciones_por_tecnico()
+        tickets_por_tecnico = self._get_tickets_por_tecnico()
 
         return {
+            'reparaciones_por_mes': reparaciones_por_mes,
+            'tickets_por_mes': tickets_por_mes,
+            'reparaciones_hoy': reparaciones_hoy,
+            'tickets_hoy': tickets_hoy,
             'total_evaluaciones': total_evaluaciones,
-            'total_reparaciones': total_reparaciones,
-            'total_alquileres': total_alquileres,
-            'total_maquinas_alquiler': total_maquinas_alquiler,
-            'total_reparaciones_hoy': total_reparaciones_hoy,
-            'total_alquileres_hoy': total_alquileres_hoy,
-            # Campos de coste/ingreso/beneficio si decides implementarlos más adelante
-            'total_costes': 0,
-            'total_ingresos': 0,
-            'total_beneficio': 0,
+            'reparaciones_por_tecnico': reparaciones_por_tecnico,
+            'tickets_por_tecnico': tickets_por_tecnico,
         }
+
+    def _get_data_por_mes(self, model, date_field, year):
+        query = f"""
+            SELECT EXTRACT(MONTH FROM {date_field}) as mes, COUNT(id)
+            FROM {self.env[model]._table}
+            WHERE EXTRACT(YEAR FROM {date_field}) = %s
+            GROUP BY mes
+        """
+        self.env.cr.execute(query, (year,))
+        return dict(self.env.cr.fetchall())
+
+    def _get_reparaciones_por_tecnico(self):
+        query = """
+            SELECT res_users.name, COUNT(reparaciones_reparaciones.id)
+            FROM reparaciones_reparaciones
+            JOIN res_users ON reparaciones_reparaciones.responsable_id = res_users.id
+            GROUP BY res_users.name
+        """
+        self.env.cr.execute(query)
+        return dict(self.env.cr.fetchall())
+
+    def _get_tickets_por_tecnico(self):
+        query = """
+            SELECT res_users.name, COUNT(ticket_alquiler.id)
+            FROM ticket_alquiler
+            JOIN res_users ON ticket_alquiler.responsable = res_users.id
+            GROUP BY res_users.name
+        """
+        self.env.cr.execute(query)
+        return dict(self.env.cr.fetchall())
