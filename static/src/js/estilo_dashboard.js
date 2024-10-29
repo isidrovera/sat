bb45fde6d1d1f756ee7766c69a9e95abe931261a
isidrovera <118472736+isidrovera@users.noreply.github.com>
2024-10-29 11:29:02 -0500

    ...

--- static/src/js/estilo_dashboard.js
+++ static/src/js/estilo_dashboard.js
@@ -10,16 +10,10 @@ export class Dashboard extends Component {
 
     static template = 'sat.DashboardTemplate';
 
-    /**
-     * @override
-     */
     async mounted() {
         this.setupChartExpansion();
     }
 
-    /**
-     * Configura los listeners para expandir/contraer gráficos
-     */
     setupChartExpansion() {
         const expandButtons = this.el.querySelectorAll('.expand-button');
         expandButtons.forEach(button => {
@@ -27,10 +21,6 @@ export class Dashboard extends Component {
         });
     }
 
-    /**
-     * Alterna la expansión de un gráfico
-     * @param {HTMLElement} button - Botón de expansión clickeado
-     */
     toggleChart(button) {
         const container = button.closest('.chart-wrapper').querySelector('.chart-container');
         const icon = button.querySelector('i');
@@ -45,9 +35,9 @@ export class Dashboard extends Component {
             icon.classList.add('fa-expand');
         }
         
-        // Actualizar el gráfico después de la transición
+        // Ajusta el gráfico después de la transición
         setTimeout(() => {
-            const chartId = container.querySelector('[id^="chart"]'); // Selecciona el primer gráfico con id que contenga 'chart'
+            const chartId = container.querySelector('[id^="chart"]');
             if (this.charts[chartId]) {
                 this.charts[chartId].resize();
             }
