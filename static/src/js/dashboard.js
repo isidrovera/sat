f8d6f7a37737f438e98ea98c3242dde7be28af5b
isidrovera <118472736+isidrovera@users.noreply.github.com>
2024-10-29 09:27:26 -0500

    Update dashboard.js

--- static/src/js/dashboard.js
+++ static/src/js/dashboard.js
@@ -166,7 +166,7 @@ class SatDashboard extends Component {
                 console.log('Gráfico de disponibilidad renderizado exitosamente');
             }
             
-            // Gráfico de estado
+           // Gráfico de estado
             const estadoCtx = this._getChartContext("estadoChart");
             if (estadoCtx) {
                 console.log('Renderizando gráfico de estado...');
@@ -199,22 +199,22 @@ class SatDashboard extends Component {
                         },
                         plugins: {
                             legend: {
-                                display: true, // Muestra la leyenda
-                                position: 'right', // Coloca la leyenda a la derecha
+                                display: true,
+                                position: 'right',
                                 labels: {
-                                    boxWidth: 20, // Ancho de la caja de la leyenda
+                                    boxWidth: 20,
                                 }
                             },
                             datalabels: {
-                                // Configuración para mostrar los valores dentro del gráfico
+                                display: true, // Muestra los datos en las secciones
                                 color: 'white',
                                 font: {
                                     size: 16,
                                     weight: 'bold'
                                 },
-                                formatter: (value) => value,
-                                anchor: 'end', // Ancla la etiqueta al final
-                                align: 'end', // Alinea la etiqueta al final
+                                formatter: (value) => value, // Muestra el valor numérico
+                                anchor: 'center', // Posiciona la etiqueta en el centro de cada sección
+                                align: 'center'  // Centra el texto dentro de cada sección
                             }
                         }
                     },
@@ -237,6 +237,7 @@ class SatDashboard extends Component {
                 });
                 console.log('Gráfico de estado renderizado exitosamente');
             }
+
             // Gráfico de técnicos
             const tecnicosCtx = this._getChartContext("tecnicosChart");
             if (tecnicosCtx) {
