8ceb6549ae079734f7d80adae5c9ad4d340f3619
isidrovera <118472736+isidrovera@users.noreply.github.com>
2024-11-05 22:40:41 -0500

    Actualizar status_widget.js

--- static/src/js/status_widget.js
+++ static/src/js/status_widget.js
@@ -1,68 +1,48 @@
-// Custom Status Widget JavaScript (static/src/js/status_widget.js)
-odoo.define('sat.StatusWidget', function (require) {
-    "use strict";
+/** @odoo-module **/
 
-    const AbstractField = require('web.AbstractField');
-    const registry = require('web.field_registry');
-    const core = require('web.core');
-    const QWeb = core.qweb;
+import { registry } from "@web/core/registry";
+import { standardFieldProps } from "@web/views/fields/standard_field_props";
+import { Field } from "@web/views/fields/field";
 
-    const StatusWidget = AbstractField.extend({
-        template: 'StatusWidgetTemplate',
-        events: {
-            'click .status-option': '_onStatusClick',
-            'click .current-status': '_toggleDropdown'
-        },
-        
-        init: function () {
-            this._super.apply(this, arguments);
-            this.status_colors = {
-                'sin_revisar': '#grey',
-                'para_revision': '#blue',
-                'asignado': '#orange',
-                'en_revision': '#yellow',
-                'finalizado': '#green',
-                'con_problemas': '#red',
-                'de_partes': '#purple',
-                'entregada': '#teal'
-            };
-        },
+class StatusWidget extends Field {
+    setup() {
+        super.setup();
+        this.status_colors = {
+            'sin_revisar': '#808080',
+            'para_revision': '#3498db',
+            'asignado': '#f39c12',
+            'en_revision': '#f1c40f',
+            'finalizado': '#2ecc71',
+            'con_problemas': '#e74c3c',
+            'de_partes': '#9b59b6',
+            'entregada': '#1abc9c'
+        };
+    }
 
-        _render: function () {
-            this.$el.html(QWeb.render('StatusWidgetTemplate', {
-                status_colors: this.status_colors,
-                current_value: this.value,
-                selection: this.field.selection,
-                readonly: this.mode === 'readonly'
-            }));
-            this._setStatusColor();
-        },
+    get statusColor() {
+        return this.status_colors[this.props.value] || '#808080';
+    }
 
-        _setStatusColor: function () {
-            const color = this.status_colors[this.value];
-            this.$('.status-bar').css('background-color', color);
-        },
+    async onChange(newValue) {
+        await this.props.update(newValue);
+    }
 
-        _toggleDropdown: function () {
-            if (this.mode !== 'readonly') {
-                this.$('.status-dropdown').toggleClass('show');
-            }
-        },
+    toggleDropdown() {
+        if (!this.props.readonly) {
+            const dropdown = this.el.querySelector('.status-dropdown');
+            dropdown.classList.toggle('show');
+        }
+    }
+}
 
-        _onStatusClick: function (ev) {
-            if (this.mode === 'readonly') {
-                return;
-            }
-            const newValue = $(ev.currentTarget).data('value');
-            this._setValue(newValue);
-            this.$('.status-dropdown').removeClass('show');
-        },
+StatusWidget.template = 'StatusWidgetTemplate';
+StatusWidget.props = {
+    ...standardFieldProps,
+    record: { type: Object },
+};
 
-        isSet: function () {
-            return true;
-        },
-    });
+StatusWidget.supportedTypes = ['selection'];
 
-    registry.add('status_widget', StatusWidget);
-    return StatusWidget;
-});
+registry.category("fields").add("status_widget", StatusWidget);
+
+export default StatusWidget;
