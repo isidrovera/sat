396081f6781d5b761828fd8cff8d0aee54f96ba7
isidrovera <118472736+isidrovera@users.noreply.github.com>
2024-11-06 13:19:45 -0500

    ..

--- static/src/js/estado_color.js
+++ static/src/js/estado_color.js
@@ -1,19 +1,41 @@
 /** @odoo-module **/
+
 import { registry } from "@web/core/registry";
 import { StatusBar } from "@web/views/fields/status_bar/status_bar";
 
-registry.category("fields").add("colored_statusbar", StatusBar.extend({
-    _render() {
-        this._super.apply(this, arguments);
+export class ColoredStatusBar extends StatusBar {
+    setup() {
+        super.setup();
+    }
 
-        // Limpiar clases previas
-        this.el.classList.remove("estado-sin_revisar", "estado-para_revision", "estado-asignado",
-                                 "estado-en_revision", "estado-finalizado", "estado-con_problemas",
-                                 "estado-de_partes", "estado-entregada");
+    get colorClass() {
+        const value = this.props.value;
+        if (!value) return '';
+        return `estado-${value}`;
+    }
 
-        // Agregar clase basada en el valor del estado
-        if (this.value) {
-            this.el.classList.add(`estado-${this.value}`);
+    // Override del método de renderizado
+    onPatched() {
+        super.onPatched();
+        
+        // Remover clases anteriores
+        this.el.classList.remove(
+            "estado-sin_revisar",
+            "estado-para_revision",
+            "estado-asignado",
+            "estado-en_revision",
+            "estado-finalizado",
+            "estado-con_problemas",
+            "estado-de_partes",
+            "estado-entregada"
+        );
+
+        // Agregar la nueva clase
+        if (this.props.value) {
+            this.el.classList.add(`estado-${this.props.value}`);
         }
-    },
-}));
+    }
+}
+
+// Registrar el campo personalizado
+registry.category("fields").add("colored_statusbar", ColoredStatusBar);
