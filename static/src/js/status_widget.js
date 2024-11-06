70c58b5419724f0ebc5748c441b50a35be483d52
isidrovera <118472736+isidrovera@users.noreply.github.com>
2024-11-06 08:38:21 -0500

    ..

--- static/src/js/status_widget.js
+++ static/src/js/status_widget.js
@@ -1,54 +1,67 @@
 /** @odoo-module **/
 
 import { registry } from "@web/core/registry";
+import { standardFieldProps } from "@web/views/fields/standard_field_props";
 import { Field } from "@web/views/fields/field";
-const { Component } = owl;
 
-export class StatusField extends Component {
-    static template = 'sat.StatusField';
-    
+class StatusWidget extends Field {
     setup() {
+        super.setup();
         this.status_config = {
             'sin_revisar': {
                 color: '#E0E0E0',
-                icon: 'fa-clock-o',
-                bgColor: '#F5F5F5'
+                icon: 'fa fa-clock-o',
+                bgColor: '#F5F5F5',
+                label: 'Sin Revisar'
             },
             'para_revision': {
                 color: '#3498db',
-                icon: 'fa-search',
-                bgColor: '#EBF5FB'
+                icon: 'fa fa-search',
+                bgColor: '#EBF5FB',
+                label: 'Para Revisión'
             },
             'asignado': {
                 color: '#f39c12',
-                icon: 'fa-user',
-                bgColor: '#FEF5E7'
+                icon: 'fa fa-user',
+                bgColor: '#FEF5E7',
+                label: 'Asignado'
             },
             'en_revision': {
                 color: '#f1c40f',
-                icon: 'fa-cogs',
-                bgColor: '#FEF9E7'
+                icon: 'fa fa-cogs',
+                bgColor: '#FEF9E7',
+                label: 'En Revisión'
             },
             'finalizado': {
                 color: '#2ecc71',
-                icon: 'fa-check-circle',
-                bgColor: '#E8F8F5'
+                icon: 'fa fa-check-circle',
+                bgColor: '#E8F8F5',
+                label: 'Finalizado'
             },
             'con_problemas': {
                 color: '#e74c3c',
-                icon: 'fa-exclamation-triangle',
-                bgColor: '#FDEDEC'
+                icon: 'fa fa-exclamation-triangle',
+                bgColor: '#FDEDEC',
+                label: 'Con Problemas'
             },
             'de_partes': {
                 color: '#9b59b6',
-                icon: 'fa-puzzle-piece',
-                bgColor: '#F4ECF7'
+                icon: 'fa fa-puzzle-piece',
+                bgColor: '#F4ECF7',
+                label: 'De Partes'
             },
             'entregada': {
                 color: '#1abc9c',
-                icon: 'fa-handshake-o',
-                bgColor: '#E8F6F3'
+                icon: 'fa fa-handshake-o',
+                bgColor: '#E8F6F3',
+                label: 'Entregada'
             }
+        };
+    }
+
+    get options() {
+        return {
+            ...this.props.record.field.selection,
         };
     }
 
@@ -56,27 +69,19 @@ export class StatusField extends Component {
         return this.status_config[value] || this.status_config.sin_revisar;
     }
 
-    getStatusLabel(value) {
-        const selection = this.props.record.field.selection;
-        const option = selection.find(opt => opt[0] === value);
-        return option ? option[1] : '';
-    }
-
-    isActiveStatus(currentValue, optionValue) {
-        const selection = this.props.record.field.selection;
-        const currentIndex = selection.findIndex(opt => opt[0] === currentValue);
-        const optionIndex = selection.findIndex(opt => opt[0] === optionValue);
-        return optionIndex <= currentIndex;
-    }
-
-    async onStatusClick(newValue) {
+    async _onStatusClick(value) {
         if (!this.props.readonly) {
-            await this.props.update(newValue);
+            await this.props.update(value);
         }
     }
 }
 
-registry.category("fields").add("status_widget", {
-    component: StatusField,
-    supportedTypes: ["selection"],
-});
+StatusWidget.template = 'sat.StatusWidget';
+StatusWidget.props = {
+    ...standardFieldProps,
+};
+StatusWidget.supportedTypes = ['selection'];
+
+registry.category("fields").add("status_widget", StatusWidget);
+
+export default StatusWidget;
