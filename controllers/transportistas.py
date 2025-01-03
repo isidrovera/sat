cc83c1ab4078d60d9b9f9fa3e7f917e885fe3ea9
isidrovera <118472736+isidrovera@users.noreply.github.com>
2025-01-03 11:11:49 -0500

    Update transportistas.py

--- controllers/transportistas.py
+++ controllers/transportistas.py
@@ -1,24 +1,24 @@
 from odoo import http
 from odoo.http import request
 import logging
+import pytz
+from datetime import datetime
 
 _logger = logging.getLogger(__name__)
 
 class SatController(http.Controller):
-
     @http.route('/sat/change_location/<int:record_id>', type='http', auth='public', website=True)
     def change_location(self, record_id, **kwargs):
-        _logger.info(f"Request received to change location for record ID: {record_id}")
         record = request.env['sat.sat'].sudo().browse(record_id)
-        if record.exists():
-            _logger.info(f"Record found: {record.name.name} with current location {record.ubicacion_id}")
-            record.ubicacion_id = 'primer_piso'
-            _logger.info(f"Location changed to 'primer_piso' for record ID: {record_id}")
-            context = {
-                'record': record,
-                'website': request.env['website'].get_current_website() if request.env['website'].search([]) else None,
-            }
-            return request.render('sat.location_change_success', context)
-        else:
-            _logger.error(f"Record with ID {record_id} not found")
+        if not record.exists():
             return request.not_found()
+            
+        old_location = record.ubicacion_id
+        record.write({'ubicacion_id': 'primer_piso'})
+        
+        # Log cambio
+        peru_tz = pytz.timezone('America/Lima')
+        current_time = datetime.now(peru_tz).strftime('%Y-%m-%d %H:%M:%S')
+        record.message_post(body=f"Ubicación cambiada de {old_location} a primer_piso el {current_time}")
+        
+        return request.render('sat.location_change_success', {})
