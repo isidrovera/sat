7ca9a34785f314c1582521d25895022eb9307125
isidrovera <118472736+isidrovera@users.noreply.github.com>
2025-05-05 09:02:41 -0500

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -110,6 +110,7 @@
         'views/solicitud_partes_views.xml',
         'views/partes_templates_alquiler.xml',
         'views/envio_masivo_evaluaciones.xml',
+        'views/equipment_visit_report_views.xml',
         
         
     ],
