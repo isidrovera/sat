37202e707959b34cee25d5dc6aeb2cefdf2bf92f
isidrovera <118472736+isidrovera@users.noreply.github.com>
2025-05-31 12:03:46 -0500

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -128,8 +128,7 @@
             'sat/static/src/css/tree_dashboard.css',
             'sat/static/src/css/evaluation_form.css',
             'sat/static/src/css/parts_request_message.css',
-            'sat/static/src/css/image-viewer.css',
-            'sat/static/src/css/equipment_blocking.css',
+            'sat/static/src/css/image-viewer.css',            
             'sat/static/src/css/equipment_blocking_dashboard.css',
             
             
