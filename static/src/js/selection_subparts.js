0679f2d2061519a840c060003669ff748b5d67b5
isidrovera <118472736+isidrovera@users.noreply.github.com>
2025-09-16 12:27:27 -0500

    Update selection_subparts.js

--- static/src/js/selection_subparts.js
+++ static/src/js/selection_subparts.js
@@ -3,7 +3,5 @@
 import { registry } from "@web/core/registry";
 import { SelectionField } from "@web/views/fields/selection/selection_field";
 
-// Registrar alias: tu nombre → componente nativo
-registry.category("view_widgets").add("selection_subparts", {
-    component: SelectionField,
-});
+// Registrar el widget para campos tipo selection
+registry.category("fields").add("selection_subparts", SelectionField);
