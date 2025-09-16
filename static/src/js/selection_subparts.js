662da2f9b817d5c194e50fcc77861604c6266ff7
isidrovera <118472736+isidrovera@users.noreply.github.com>
2025-09-16 15:51:48 -0500

    ..

--- static/src/js/selection_subparts.js
+++ static/src/js/selection_subparts.js
@@ -1,23 +1,50 @@
 /** @odoo-module **/
 
 import { registry } from "@web/core/registry";
+import { standardFieldProps } from "@web/views/fields/standard_field_props";
 import { Component } from "@odoo/owl";
 import { useService } from "@web/core/utils/hooks";
 
 export class SelectionSubparts extends Component {
-    static template = "web.SelectionField"; // Usar el template base de Odoo
-    
+    static template = "SelectionSubparts.Template";
+    static props = {
+        ...standardFieldProps,
+    };
+
     setup() {
         this.action = useService("action");
         this.notification = useService("notification");
     }
-    
+
+    get fieldInfo() {
+        return this.props.record.fields[this.props.name];
+    }
+
+    get selection() {
+        return this.fieldInfo.selection || [];
+    }
+
+    get value() {
+        return this.props.record.data[this.props.name] || "";
+    }
+
+    get displayValue() {
+        const selection = this.selection;
+        for (const [key, label] of selection) {
+            if (key === this.value) {
+                return label;
+            }
+        }
+        return "";
+    }
+
     async onChange(ev) {
         const newValue = ev.target.value;
         
         // Actualizar el valor en el record
         await this.props.record.update({ [this.props.name]: newValue });
         
+        // Solo proceder si el valor es "requiere_cambio"
         if (newValue === "requiere_cambio") {
             const resId = this.props.record.resId;
             
@@ -29,19 +56,28 @@ export class SelectionSubparts extends Component {
                 return;
             }
 
-            await this.action.doAction({
-                type: "ir.actions.act_window",
-                name: "Añadir/Editar Subpartes", 
-                res_model: "reparacion.add.subparts.wizard",
-                target: "new",
-                views: [[false, "form"]],
-                context: {
-                    default_reparacion_id: resId,
-                    active_id: resId,
-                },
-            });
+            // Abrir wizard
+            try {
+                await this.action.doAction({
+                    type: "ir.actions.act_window",
+                    name: "Añadir/Editar Subpartes",
+                    res_model: "reparacion.add.subparts.wizard",
+                    target: "new",
+                    views: [[false, "form"]],
+                    context: {
+                        default_reparacion_id: resId,
+                        active_id: resId,
+                    },
+                });
+            } catch (error) {
+                this.notification.add(
+                    "Error abriendo el wizard: " + error.message,
+                    { type: "danger" }
+                );
+            }
         }
     }
 }
 
+// Registrar el widget
 registry.category("fields").add("selection_subparts", SelectionSubparts);
