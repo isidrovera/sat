3a973630bc5c47dbdebda219429f75b4233c061b
isidrovera <118472736+isidrovera@users.noreply.github.com>
2025-10-10 08:46:42 -0500

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -67,4 +67,4 @@ from . import componente_color
 from . import reparacion_componente_evaluacion
 from . import reparaciones_migration
 from . import soporte_mensajes
-from . import 
+from . import ticket_informes_mixing
