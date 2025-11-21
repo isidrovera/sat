d786bd8a9847691d09cb664875603a40a96edeab
isidrovera <118472736+isidrovera@users.noreply.github.com>
2025-11-21 17:19:21 -0500

    ...

--- static/src/js/sat_dashboard.js
+++ static/src/js/sat_dashboard.js
@@ -4,40 +4,52 @@ import { ListController } from "@web/views/list/list_controller";
 import { listView } from "@web/views/list/list_view";
 import { registry } from "@web/core/registry";
 import { useService } from "@web/core/utils/hooks";
-import { Component, onWillStart, useState } from "@odoo/owl";
+import { onWillStart, onWillUpdateProps, useState } from "@odoo/owl";
 
 export class SatListController extends ListController {
     setup() {
         super.setup();
         this.orm = useService("orm");
-        this.dashboardData = useState({ values: {} });
+        this.dashboardData = useState({});
         
         onWillStart(async () => {
+            await this.loadDashboardData();
+        });
+
+        onWillUpdateProps(async () => {
             await this.loadDashboardData();
         });
     }
 
     async loadDashboardData() {
-        const domain = this.model.root.domain || [];
-        const result = await this.orm.call(
-            "sat.sat",
-            "get_sat_dashboard_values",
-            [],
-            { domain: domain }
-        );
-        this.dashboardData.values = result;
-    }
+        try {
+            // Obtener el dominio de forma segura
+            let domain = [];
+            if (this.model && this.model.root) {
+                domain = this.model.root.domain || [];
+            } else if (this.props && this.props.domain) {
+                domain = this.props.domain;
+            }
 
-    async onUpdated() {
-        await super.onUpdated(...arguments);
-        await this.loadDashboardData();
+            const result = await this.orm.call(
+                "sat.sat",
+                "get_sat_dashboard_values",
+                [],
+                { domain: domain }
+            );
+            
+            Object.assign(this.dashboardData, result);
+        } catch (error) {
+            console.error("Error loading SAT dashboard:", error);
+        }
     }
 }
+
+SatListController.template = "sat.SatSatDashboard";
 
 export const satListView = {
     ...listView,
     Controller: SatListController,
-    buttonTemplate: "sat.SatSatDashboard",
 };
 
 registry.category("views").add("sat_tree_dashboard", satListView);
